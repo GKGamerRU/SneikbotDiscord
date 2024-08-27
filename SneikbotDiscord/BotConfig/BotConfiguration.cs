132575e504317a6c71173e7a7d8c5d4002a7a318
GKGamerRU <superkan224@gmail.com>
2024-08-28 01:05:36 +0300

    Добавлена проверка на наличие файла конфигурации бота.

--- SneikbotDiscord/BotConfig/BotConfiguration.cs
+++ SneikbotDiscord/BotConfig/BotConfiguration.cs
@@ -17,8 +17,21 @@ namespace SneikbotDiscord.BotConfig
         public static BotConfiguration LoadConfig()
         {
             var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "config.json");
-            var json = JsonConvert.DeserializeObject<BotConfiguration>(File.ReadAllText(path));
-            return json;
+            if (File.Exists(path))
+            {
+                var json = JsonConvert.DeserializeObject<BotConfiguration>(File.ReadAllText(path));
+                return json;
+            }
+            else
+            {
+                var newConfig = new BotConfiguration();
+                newConfig.Token = "YOUR_BOT_TOKEN_HERE";
+                newConfig.Prefix = "!";
+                MessageBox.Show("Файл конфигурации config.json не найден, но был создан. Пожалуйста, Введите данные в этот файл.");
+
+                File.WriteAllText(path, JsonConvert.SerializeObject(newConfig));
+                return newConfig;
+            }
         }
     }
 }
